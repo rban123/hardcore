6c9e6a35c8b8dd021268fc3cc7c980b6d5eca821
Ryan Bandler <ryanbandler@gmail.com>
2024-04-24 16:36:48 -0500

    built bot

---
+++ bot.js
@@ -0,0 +1,117 @@
+import "dotenv/config"
+
+import { REST, Routes } from 'discord.js';
+import { Client, GatewayIntentBits } from 'discord.js';
+import { Translate } from '@google-cloud/translate/build/src/v2/index.js';
+
+const constants = {
+    DISCORD_TOKEN: process.env.DISCORD_TOKEN,
+    CLIENT_ID: process.env.CLIENT_ID,
+    GOOGLE_PROJ_ID: process.env.GOOGLE_PROJ_ID,
+    GOOGLE_API_KEY: process.env.GOOGLE_API_KEY   
+}
+
+const rest = new REST({ version: '10' }).setToken(constants.DISCORD_TOKEN)
+
+const client = new Client({ intents: [
+    GatewayIntentBits.Guilds, 
+    GatewayIntentBits.GuildMessages, 
+    GatewayIntentBits.GuildMembers, 
+    GatewayIntentBits.MessageContent
+]});
+
+const translate = new Translate({
+    projectId: constants.GOOGLE_PROJ_ID, 
+    key: constants.GOOGLE_API_KEY
+});
+
+const LANG_ROLE_MAP = {
+    "English Native": "en",
+    "Spanish Native": "es"
+}
+
+async function detectLanguage(text) {
+  let [detections] = await translate.detect(text);
+  detections = Array.isArray(detections) ? detections : [detections];
+  return detections[0]
+}
+
+const COMMANDS = [
+    {
+      name: 'hardcore',
+      description: 'Toggles hardcore mode!',
+    },
+  ];
+
+const refreshCommands = async(commands) => {
+    console.log('Started refreshing application (/) commands.');
+      
+    await rest.put(
+        Routes.applicationCommands(constants.CLIENT_ID), 
+        { body: commands }
+    );
+  
+    console.log('Successfully reloaded application (/) commands.');
+}
+
+const checkRoles = (member, roleName) => {
+    return member.roles.cache.some(role => role.name === roleName)
+}
+
+const handleMessages = async(message) => {
+    const USER_ID = message.author.id
+    const USER_NAME = message.author.username
+    const IS_BOT = message.author.bot
+    const isHardcore = checkRoles(message.member, "Hardcore")
+
+    if(IS_BOT){
+        console.log(`bot user #${USER_ID} (${USER_NAME}) ignored`)
+        return
+    } else if (isHardcore) {
+        const detectionResults = await detectLanguage(message.content)
+        const language = detectionResults.language
+
+        if(message.member.roles.cache.some(role => LANG_ROLE_MAP[role.name] === language))
+            message.delete()
+    
+    } else {
+        console.info(`user #${USER_ID} (${USER_NAME}) does not have hardcore mode activated`)
+        return
+    }
+}
+
+const handleInteractions = async(interaction) => {
+    if (!interaction.isChatInputCommand()) return;
+      
+    console.log(checkRoles(interaction.member, "Hardcore"))
+
+    if (interaction.commandName === 'hardcore') {
+        if(checkRoles(interaction.member, "Hardcore")){
+
+            await interaction.member.roles.remove("1232715390539403375")
+            await interaction.reply("hardcore has been removed!")
+        } else {
+            await interaction.member.roles.add("1232715390539403375") 
+            await interaction.reply("hardcore has been added!")
+        }
+    }
+}
+
+const main = async() => {
+    try {
+        refreshCommands(COMMANDS)
+    
+        client.on('ready', () => {
+            console.log(`Logged in as ${client.user.tag}!`);
+        });
+        
+        client.on('messageCreate', handleMessages)
+        client.on('interactionCreate', handleInteractions);
+        
+        client.login(constants.DISCORD_TOKEN);
+    } catch (e) {
+        console.log(e)
+    }
+}
+
+main()
